d975546d9b1f5505a682a8622832dcffeb0a6687
mattatti <matan.atti@gmail.com>
2018-06-26 11:34:15 +0300

    Add files via upload

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using RentAMovie.Dtos;
+using RentAMovie.Models;
+
+namespace RentAMovie.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+            Mapper.CreateMap<Genre, GenreDto>();
+            Mapper.CreateMap<Rental, RentalDto>();
+
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<RentalDto, Rental>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+        }
+    }
+}
